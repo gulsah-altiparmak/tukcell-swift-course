54ae7f70347b965a6f35809852ae9dcb867b1baf
gulsah-altiparmak <gulsahaltiparmakk@gmail.com>
2021-03-06 15:51:30 +0300

    13-14 Days

---
+++ turkcell/14-Days/14-Days/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  14-Days
+//
+//  Created by Gulsah Altiparmak on 2.02.2021.
+//
+
+import UIKit
+import ProgressHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //ProgressHUD.show()
+        //ProgressHUD.showSuccess()
+        ProgressHUD.showProgress(0.42)
+        
+    }
+
+
+}
+
